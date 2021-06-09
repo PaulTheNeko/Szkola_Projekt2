2c630c74d659dff539c722c6c86130cb026de427
Paultheneko <tygrysekpawelpiotr1@gmail.com>
2021-06-10 01:04:43 +0200

    yyy

--- Program.cs
+++ Program.cs
@@ -178,11 +178,11 @@ namespace PP_Projekt_1_Tic_Tac_Toe
             }
         }
 
-        (int, int) NumToPos(int num)
+        int[] NumToPos(int num)
         {
             int x = (num - 1) % 3;
             int y = (9 - num) / 3;
-            return (x,y);
+            return new int[] {x,y};
         }
 
         public void ReadMove()
@@ -191,8 +191,8 @@ namespace PP_Projekt_1_Tic_Tac_Toe
             while (true)
             {
                 int num = ReadMoveKey();
-                (int, int) m = NumToPos(num);
-                if (Move(m.Item1, m.Item2))
+                int[] m = NumToPos(num);
+                if (Move(m[0], m[1]))
                     break;
                 else
                     Console.WriteLine("Podane pole jest już zajęte");
